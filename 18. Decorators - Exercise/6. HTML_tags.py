717760480d029c3b3bd537c0d0c9a804067f51e0
Valentin Vakrilov <129154663+Valentin-Vakrilov@users.noreply.github.com>
2023-09-15 16:39:51 +0300

    Add files via upload

---
+++ 18. Decorators - Exercise/6. HTML_tags.py
@@ -0,0 +1,23 @@
+def tags(html_tag):
+    def decorator(function):
+        def wrapper(*args):
+            return f"<{html_tag}>{function(*args)}</{html_tag}>"
+
+        return wrapper
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+
+
+print(to_upper('hello'))
